c0a35780144c839774c0cff4a8ba43e2e61db1d8
kuboryu <kbtrg.527@gmail.com>
2023-03-19 03:50:02 +0900

    feat: 🎸 head情報追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
 import type { NextPage } from 'next'
 import { Heading } from '@chakra-ui/react'
+import Head from 'next/head'
 
 const Page: NextPage = () => {
-  return <Heading>Chakra UI</Heading>
+  return (
+    <div>
+      <Head>
+        <title>firebaseチャット</title>
+        <meta name="description" content="課題用のチャットアプリです。" />
+        <meta property="og:title" content="firebaseチャット" />
+        <meta property="og:description" content="課題用のチャットアプリです。" />
+        <meta property="og:image" content="/ms-library/images/og-image.png" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta httpEquiv="content-language" content="ja" />
+        <link rel="shortcut icon" href="/ms-library/favicon.ico" />
+      </Head>
+      <Heading>Chakra UI</Heading>
+    </div>
+  )
 }
 
 export default Page
