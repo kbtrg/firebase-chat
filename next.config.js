dff63bb384820a48bc2c6b6512b5970fdfece8bf
kuboryu <kbtrg.527@gmail.com>
2023-03-23 10:52:25 +0900

    fix: 🐛 [PWA]cache削除

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-const runtimeCaching = require('next-pwa/cache')
+// const runtimeCaching = require('next-pwa/cache')
 
 const withPWA = require('next-pwa')({
   dest: 'public',
   sw: "sw.js",
-  // sw: 'service-worker.js',
   disable: process.env.NODE_ENV === 'development',
-  runtimeCaching,
+  // runtimeCaching,
   buildExcludes: [/middleware-manifest.json$/],
 })
 
