f3dbb9f7725700b8fadf6f4f75f9df613fc71b58
kuboryu <kbtrg.527@gmail.com>
2023-03-23 10:05:44 +0900

    feat: 🎸 モバイルPWA化

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,12 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
+          <link rel="manifest" href="/manifest.webmanifest" />
           <link rel="apple-touch-icon" href="/icon-192x192.png" />
+          <meta name='application-name' content='ChatApp' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='チャットアプリ' />
           <meta name="theme-color" content="#042940" />
         </Head>
         <body>
