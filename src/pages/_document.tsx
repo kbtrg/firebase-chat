5996dc9b728244e45f8db8b158bd589652e8b377
kuboryu <kbtrg.527@gmail.com>
2023-03-23 18:25:36 +0900

    feat: 🎸 PWA最終調整

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,15 +14,6 @@ class MyDocument extends Document {
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <meta httpEquiv="content-language" content="ja" />
 
-          {/* <link rel="manifest" href="/manifest.json" />
-          <link rel="manifest" href="/manifest.webmanifest" />
-          <link rel="apple-touch-icon" href="/icon-192x192.png" />
-          <meta name='application-name' content='ChatApp' />
-          <meta name='apple-mobile-web-app-capable' content='yes' />
-          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-          <meta name='apple-mobile-web-app-title' content='チャットアプリ' />
-        <meta name="theme-color" content="#042940" /> */}
-
           <meta name="application-name" content="ChatApp" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
@@ -32,7 +23,7 @@ class MyDocument extends Document {
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="msapplication-TileColor" content="#2B5797" />
           <meta name="msapplication-tap-highlight" content="no" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" content="#042940" />
 
           <link rel="manifest" href="/manifest.json" />
           <link rel="manifest" href="/manifest.webmanifest" />
