44ef4d01fdb4833b2fc03720cf36e33ab55b74e7
kuboryu <kbtrg.527@gmail.com>
2023-03-23 10:40:52 +0900

    feat: 🎸 PWAのproduction環境対応

---
+++ src/pages/fallback.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Fallback = () => {
+  return(
+    <div>
+      <h1>This is fallback page when device is offline </h1>
+      <small>Route will fallback to this page</small>
+    </div>
+  )
+}
+
+export default Fallback;
