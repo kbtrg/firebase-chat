4f3651842e7d3c7bab577fefd2147e522f6b44be
kuboryu <kbtrg.527@gmail.com>
2023-03-23 10:17:00 +0900

    feat: 🎸 PWAのvercel対応

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+const runtimeCaching = require('next-pwa/cache')
+
 const withPWA = require('next-pwa')({
   dest: 'public',
+  sw: "sw.js",
   disable: process.env.NODE_ENV === 'development',
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
 })
 
 module.exports = withPWA({
