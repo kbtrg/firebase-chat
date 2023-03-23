126fbc350107f2ec79fc2cc053c47c44c553fbd6
kuboryu <kbtrg.527@gmail.com>
2023-03-23 18:01:32 +0900

    fix: 🐛 [next.config]]buildExcludes削除

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   runtimeCaching,
-  buildExcludes: [/middleware-manifest.json$/],
+  // buildExcludes: [/middleware-manifest.json$/],
 })
 
 module.exports = withPWA({
